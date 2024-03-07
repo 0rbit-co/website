23a917396325cb6480a611ca7f0b6318d778b6a2
Sarthak <notsarthakshah@gmail.com>
2024-03-07 14:16:29 +0530

    quest link updated

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -15,6 +15,10 @@ function FloatingNavDemo() {
       name: "Docs",
       link: "https://docs.0rbit.co/",
     },
+    {
+      name: "Quests",
+      link: "https://github.com/0rbit-co/quest/blob/main/quest.md",
+    }
   ];
   return (
     <div className="relative w-full">
