2765ac0f62df62b8ed631768399437044badc3b6
L-Pircy <taveesha.agarwal@gmail.com>
2024-05-28 21:17:23 +0530

    disc route

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/discord",
+        destination: "https://discord.gg/NTuWJXvJw2",
+        permanent: true, // This indicates a 301 permanent redirect. Set to false for a 307 temporary redirect.
+      },
+      // You can add more redirects here
+    ];
+  },
+};
 
 export default nextConfig;
