8a6cd59ea4bc608b9ae4a534daef18d5464a9239
Sarthak <notsarthakshah@gmail.com>
2024-06-02 11:06:35 +0530

    fix: code quality

--- app/_components/_layout/Footer.tsx
+++ app/_components/_layout/Footer.tsx
@@ -23,7 +23,7 @@ const Footer = () => {
    }
  `}
     >
-      <div className="flex flex-col lg:flex-row items-end justify-end gap-[9px]">
+      <div className="flex flex-col lg:flex-row items-end justify-end md:gap-[9px]">
         <Image
           width={60}
           height={60}
@@ -47,17 +47,17 @@ const Footer = () => {
       <div className="flex md:hidden flex-row md:gap-6 gap-[6px] items-center justify-center">
         <Link href="https://twitter.com/0rbitco">
           <FaTwitter
-            className={`${brandDarkText} hover:opacity-75 md:w-6 md:h-6 w-[15px] h-[15px]`}
+            className={`${brandDarkText} hover:opacity-75 md:w-6 md:h-6 w-[20px] h-[20px]`}
           />
         </Link>
         <Link href="https://discord.gg/JVSjqaKJgV">
           <FaDiscord
-            className={`${brandDarkText} hover:opacity-75 md:w-6 md:h-6 w-[15px] h-[15px]`}
+            className={`${brandDarkText} hover:opacity-75 md:w-6 md:h-6 w-[20px] h-[20px]`}
           />
         </Link>
         <Link href="https://github.com/0rbit-co">
           <TbBrandGithubFilled
-            className={`${brandDarkText} hover:opacity-75 md:w-6 md:h-6 w-[15px] h-[15px]`}
+            className={`${brandDarkText} hover:opacity-75 md:w-6 md:h-6 w-[20px] h-[20px]`}
           />
         </Link>
       </div>
@@ -68,35 +68,35 @@ const Footer = () => {
         >
           <Link
             href="https://docs.0rbit.co/"
-            className={`flex flex-row gap-[3px] hover:text-[#EB8F44] hover:tracking-wider hover:underline`}
+            className={`flex flex-row gap-[3px] text-md hover:text-[#EB8F44] hover:tracking-wider hover:underline`}
           >
             <h4>Docs</h4>
             <Image width={8} height={8} src={arrDark} alt="arrow" />
           </Link>
           <Link
             href="https://playground.0rbit.co/"
-            className={`flex flex-row gap-[3px] hover:text-[#EB8F44] hover:tracking-wider hover:underline`}
+            className={`flex flex-row gap-[3px] text-md hover:text-[#EB8F44] hover:tracking-wider hover:underline`}
           >
             <h4>Playground</h4>
             <Image width={8} height={8} src={arrDark} alt="arrow" />
           </Link>
           <Link
             href="/"
-            className={`flex flex-row gap-[3px] hover:text-[#EB8F44] hover:tracking-wider hover:underline`}
+            className={`flex flex-row gap-[3px] text-md hover:text-[#EB8F44] hover:tracking-wider hover:underline`}
           >
             <h4>Home</h4>
             <Image width={8} height={8} src={arrDark} alt="arrow" />
           </Link>
           <Link
             href="/quests"
-            className={`flex flex-row gap-[3px] hover:text-[#EB8F44] hover:tracking-wider hover:underline`}
+            className={`flex flex-row gap-[3px] text-md hover:text-[#EB8F44] hover:tracking-wider hover:underline`}
           >
             <h4>Quest</h4>
             <Image width={8} height={8} src={arrDark} alt="arrow" />
           </Link>
           <Link
             href="/team"
-            className={`flex flex-row gap-[3px] hover:text-[#EB8F44] hover:tracking-wider hover:underline`}
+            className={`flex flex-row gap-[3px] text-md hover:text-[#EB8F44] hover:tracking-wider hover:underline`}
           >
             <h4>Team</h4>
             <Image width={8} height={8} src={arrDark} alt="arrow" />
