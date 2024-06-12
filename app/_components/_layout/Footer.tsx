9c02434564b81aa5cba365315cb4407da5a5a6b6
Sarthak <notsarthakshah@gmail.com>
2024-06-12 12:48:39 +0530

    add: google analytics events for each element

--- app/_components/_layout/Footer.tsx
+++ app/_components/_layout/Footer.tsx
@@ -9,8 +9,26 @@ import { FaDiscord } from "react-icons/fa";
 import arrDark from "@/public/icons/linkArrow.svg";
 import { usePathname } from "next/navigation";
 
+const event = ({ action, category, label, value }: any) => {
+  (window as any).gtag('event', action, {
+    event_category: category,
+    event_label: label,
+    value: value,
+  });
+};
+
 const Footer = () => {
   const path = usePathname();
+
+  const handleLinkClick = (label: string, value: number) => {
+    event({
+      action: 'click_footer_link',
+      category: 'Navigation',
+      label,
+      value,
+    });
+  };
+
   return (
     <footer
       className={`flex flex-col md:flex-row gap-3 md:items-center items-center justify-between bottom-0 
@@ -45,17 +63,17 @@ const Footer = () => {
         </h4>
       </div>
       <div className="flex md:hidden flex-row md:gap-6 gap-[6px] items-center justify-center">
-        <Link href="https://twitter.com/0rbitco">
+        <Link href="https://twitter.com/0rbitco" onClick={() => handleLinkClick('Twitter', 1)}>
           <FaTwitter
             className={`${brandDarkText} hover:opacity-75 md:w-6 md:h-6 w-[20px] h-[20px]`}
           />
         </Link>
-        <Link href="https://discord.gg/JVSjqaKJgV">
+        <Link href="https://discord.gg/JVSjqaKJgV" onClick={() => handleLinkClick('Discord', 2)}>
           <FaDiscord
             className={`${brandDarkText} hover:opacity-75 md:w-6 md:h-6 w-[20px] h-[20px]`}
           />
         </Link>
-        <Link href="https://github.com/0rbit-co">
+        <Link href="https://github.com/0rbit-co" onClick={() => handleLinkClick('GitHub', 3)}>
           <TbBrandGithubFilled
             className={`${brandDarkText} hover:opacity-75 md:w-6 md:h-6 w-[20px] h-[20px]`}
           />
@@ -69,6 +87,7 @@ const Footer = () => {
           <Link
             href="https://docs.0rbit.co/"
             className={`flex flex-row gap-[3px] text-md hover:text-[#EB8F44] hover:tracking-wider hover:underline`}
+            onClick={() => handleLinkClick('Docs', 4)}
           >
             <h4>Docs</h4>
             <Image width={8} height={8} src={arrDark} alt="arrow" />
@@ -76,6 +95,7 @@ const Footer = () => {
           <Link
             href="https://playground.0rbit.co/"
             className={`flex flex-row gap-[3px] text-md hover:text-[#EB8F44] hover:tracking-wider hover:underline`}
+            onClick={() => handleLinkClick('Playground', 5)}
           >
             <h4>Playground</h4>
             <Image width={8} height={8} src={arrDark} alt="arrow" />
@@ -83,6 +103,7 @@ const Footer = () => {
           <Link
             href="/"
             className={`flex flex-row gap-[3px] text-md hover:text-[#EB8F44] hover:tracking-wider hover:underline`}
+            onClick={() => handleLinkClick('Home', 6)}
           >
             <h4>Home</h4>
             <Image width={8} height={8} src={arrDark} alt="arrow" />
@@ -90,6 +111,7 @@ const Footer = () => {
           <Link
             href="/quests"
             className={`flex flex-row gap-[3px] text-md hover:text-[#EB8F44] hover:tracking-wider hover:underline`}
+            onClick={() => handleLinkClick('Quest', 7)}
           >
             <h4>Quest</h4>
             <Image width={8} height={8} src={arrDark} alt="arrow" />
@@ -97,24 +119,25 @@ const Footer = () => {
           <Link
             href="/team"
             className={`flex flex-row gap-[3px] text-md hover:text-[#EB8F44] hover:tracking-wider hover:underline`}
+            onClick={() => handleLinkClick('Team', 8)}
           >
             <h4>Team</h4>
             <Image width={8} height={8} src={arrDark} alt="arrow" />
-          </Link> 
+          </Link>
         </div>
       </div>
       <div className="md:flex hidden flex-row md:gap-6 gap-[6px] items-center justify-center">
-        <Link href="https://twitter.com/0rbitco">
+        <Link href="https://twitter.com/0rbitco" onClick={() => handleLinkClick('Twitter', 9)}>
           <FaTwitter
             className={`${brandDarkText} hover:opacity-75 md:w-6 md:h-6 w-[15px] h-[15px]`}
           />
         </Link>
-        <Link href="https://discord.gg/JVSjqaKJgV">
+        <Link href="https://discord.gg/JVSjqaKJgV" onClick={() => handleLinkClick('Discord', 10)}>
           <FaDiscord
             className={`${brandDarkText} hover:opacity-75 md:w-6 md:h-6 w-[15px] h-[15px]`}
           />
         </Link>
-        <Link href="https://github.com/0rbit-co">
+        <Link href="https://github.com/0rbit-co" onClick={() => handleLinkClick('GitHub', 11)}>
           <TbBrandGithubFilled
             className={`${brandDarkText} hover:opacity-75 md:w-6 md:h-6 w-[15px] h-[15px]`}
           />
