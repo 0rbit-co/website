ea6ee1ec73321f4d1cc25b686548b84757449586
Not-Sarthak <notsarthakshah@gmail.com>
2024-06-13 14:12:16 +0530

    fix: footer

--- app/_components/_layout/Footer.tsx
+++ app/_components/_layout/Footer.tsx
@@ -10,7 +10,7 @@ import arrDark from "@/public/icons/linkArrow.svg";
 import { usePathname } from "next/navigation";
 
 const event = ({ action, category, label, value }: any) => {
-  (window as any).gtag('event', action, {
+  (window as any).gtag("event", action, {
     event_category: category,
     event_label: label,
     value: value,
@@ -22,8 +22,8 @@ const Footer = () => {
 
   const handleLinkClick = (label: string, value: number) => {
     event({
-      action: 'click_footer_link',
-      category: 'Navigation',
+      action: "click_footer_link",
+      category: "Navigation",
       label,
       value,
     });
@@ -31,7 +31,7 @@ const Footer = () => {
 
   return (
     <footer
-      className={`flex flex-col md:flex-row gap-3 md:items-center items-center justify-between bottom-0 
+      className={`flex flex-col md:flex-row gap-1 md:gap-3 md:items-center items-center justify-between bottom-0 
        py-[12px] md:px-[30px] px-[9px] w-[100vw] max-w-[1800px]
        border-t-[1.2px] border-[#000000]
    ${
@@ -42,38 +42,47 @@ const Footer = () => {
  `}
     >
       <div className="flex flex-col lg:flex-row items-center justify-center md:justify-end md:items-end md:gap-[9px]">
-        <Image
-          width={60}
-          height={60}
-          src="/logos/recLight.svg"
-          alt="logo"
-          className="md:block hidden"
-        />
-        <Image
-          width={90}
-          height={90}
-          src="/logos/recLight.svg"
-          alt="logo"
-          className="md:hidden w-[60px] h-[50px] "
-        />
-        <h4
-          className={`${brandSecondaryText} md:text-[12px] text-[9px] tracking-tighter font-[Regular] pb-[3px]`}
-        >
-          &copy; 2024 0rbit
-        </h4>
+          <Image
+            width={60}
+            height={60}
+            src="/logos/recLight.svg"
+            alt="logo"
+            className="md:block hidden"
+          />
+          <Image
+            width={90}
+            height={90}
+            src="/logos/recLight.svg"
+            alt="logo"
+            className="md:hidden w-[60px]"
+          />
+          <h4
+            className={`${brandSecondaryText} md:text-[12px] text-[10px] font-semibold tracking-tighter font-[Regular] pb-[3px]`}
+          >
+            &copy; 2024 0rbit
+          </h4>
       </div>
-      <div className="flex md:hidden flex-row md:gap-6 gap-[6px] items-center justify-center">
-        <Link href="https://twitter.com/0rbitco" onClick={() => handleLinkClick('Twitter', 1)}>
+      <div className="flex md:hidden flex-row md:gap-6 gap-6 items-center justify-center mt-1 md:mt-0">
+        <Link
+          href="https://twitter.com/0rbitco"
+          onClick={() => handleLinkClick("Twitter", 1)}
+        >
           <FaTwitter
             className={`${brandDarkText} hover:opacity-75 md:w-6 md:h-6 w-[20px] h-[20px]`}
           />
         </Link>
-        <Link href="https://discord.gg/JVSjqaKJgV" onClick={() => handleLinkClick('Discord', 2)}>
+        <Link
+          href="https://discord.gg/JVSjqaKJgV"
+          onClick={() => handleLinkClick("Discord", 2)}
+        >
           <FaDiscord
             className={`${brandDarkText} hover:opacity-75 md:w-6 md:h-6 w-[20px] h-[20px]`}
           />
         </Link>
-        <Link href="https://github.com/0rbit-co" onClick={() => handleLinkClick('GitHub', 3)}>
+        <Link
+          href="https://github.com/0rbit-co"
+          onClick={() => handleLinkClick("GitHub", 3)}
+        >
           <TbBrandGithubFilled
             className={`${brandDarkText} hover:opacity-75 md:w-6 md:h-6 w-[20px] h-[20px]`}
           />
@@ -81,13 +90,13 @@ const Footer = () => {
       </div>
       <div className="flex flex-col justify-center items-center gap-1">
         <div
-          className="flex flex-row items-center justify-center sm:gap-6 gap-3
-      md:text-[15px] text-[9px]"
+          className="flex flex-row items-center justify-center sm:gap-6 gap-6 font-semibold mt-2
+      md:text-[15px] text-[12px]"
         >
           <Link
             href="https://docs.0rbit.co/"
             className={`flex flex-row gap-[3px] text-md hover:text-[#EB8F44] hover:tracking-wider hover:underline`}
-            onClick={() => handleLinkClick('Docs', 4)}
+            onClick={() => handleLinkClick("Docs", 4)}
           >
             <h4>Docs</h4>
             <Image width={8} height={8} src={arrDark} alt="arrow" />
@@ -95,7 +104,7 @@ const Footer = () => {
           <Link
             href="https://playground.0rbit.co/"
             className={`flex flex-row gap-[3px] text-md hover:text-[#EB8F44] hover:tracking-wider hover:underline`}
-            onClick={() => handleLinkClick('Playground', 5)}
+            onClick={() => handleLinkClick("Playground", 5)}
           >
             <h4>Playground</h4>
             <Image width={8} height={8} src={arrDark} alt="arrow" />
@@ -103,7 +112,7 @@ const Footer = () => {
           <Link
             href="/"
             className={`flex flex-row gap-[3px] text-md hover:text-[#EB8F44] hover:tracking-wider hover:underline`}
-            onClick={() => handleLinkClick('Home', 6)}
+            onClick={() => handleLinkClick("Home", 6)}
           >
             <h4>Home</h4>
             <Image width={8} height={8} src={arrDark} alt="arrow" />
@@ -111,7 +120,7 @@ const Footer = () => {
           <Link
             href="/quests"
             className={`flex flex-row gap-[3px] text-md hover:text-[#EB8F44] hover:tracking-wider hover:underline`}
-            onClick={() => handleLinkClick('Quest', 7)}
+            onClick={() => handleLinkClick("Quest", 7)}
           >
             <h4>Quest</h4>
             <Image width={8} height={8} src={arrDark} alt="arrow" />
@@ -119,7 +128,7 @@ const Footer = () => {
           <Link
             href="/team"
             className={`flex flex-row gap-[3px] text-md hover:text-[#EB8F44] hover:tracking-wider hover:underline`}
-            onClick={() => handleLinkClick('Team', 8)}
+            onClick={() => handleLinkClick("Team", 8)}
           >
             <h4>Team</h4>
             <Image width={8} height={8} src={arrDark} alt="arrow" />
@@ -127,17 +136,26 @@ const Footer = () => {
         </div>
       </div>
       <div className="md:flex hidden flex-row md:gap-6 gap-[6px] items-center justify-center">
-        <Link href="https://twitter.com/0rbitco" onClick={() => handleLinkClick('Twitter', 9)}>
+        <Link
+          href="https://twitter.com/0rbitco"
+          onClick={() => handleLinkClick("Twitter", 9)}
+        >
           <FaTwitter
             className={`${brandDarkText} hover:opacity-75 md:w-6 md:h-6 w-[15px] h-[15px]`}
           />
         </Link>
-        <Link href="https://discord.gg/JVSjqaKJgV" onClick={() => handleLinkClick('Discord', 10)}>
+        <Link
+          href="https://discord.gg/JVSjqaKJgV"
+          onClick={() => handleLinkClick("Discord", 10)}
+        >
           <FaDiscord
             className={`${brandDarkText} hover:opacity-75 md:w-6 md:h-6 w-[15px] h-[15px]`}
           />
         </Link>
-        <Link href="https://github.com/0rbit-co" onClick={() => handleLinkClick('GitHub', 11)}>
+        <Link
+          href="https://github.com/0rbit-co"
+          onClick={() => handleLinkClick("GitHub", 11)}
+        >
           <TbBrandGithubFilled
             className={`${brandDarkText} hover:opacity-75 md:w-6 md:h-6 w-[15px] h-[15px]`}
           />
