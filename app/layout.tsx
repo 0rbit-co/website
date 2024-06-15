37c090f0e2d4175d88861ac1963e62dc03beac9a
L-Pircy <taveesha.agarwal@gmail.com>
2024-06-15 20:22:57 +0530

    robots, sitemap and og logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "0rbit | Oracle on Arweave",
   description:
-    "The Decentralized Oracle Network on AO for accessing the off-chain data.",
+    "The Decentralized Oracle Network on AO for accessing any off-chain data.",
   icons: {
     icon: [
       {
